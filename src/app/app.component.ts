6840c9bd5e128663c0c308abe53bf153648f887d
uriel <uriel_enri@outlook.com>
2021-05-03 18:19:42 -0500

    piano app

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pianoapp';
+
+
+  getSonido(numero: number){
+    const audio = new Audio();
+    audio.src = 'assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+  }
+
+}
